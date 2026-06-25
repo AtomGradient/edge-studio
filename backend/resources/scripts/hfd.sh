4bd9ac7b7b6391031f5d7435dea1af7f3b6689ff
wushuhong <ws169144@gmail.com>
2026-06-25 21:10:07 +0800

    fix:copyright

--- backend/resources/scripts/hfd.sh
+++ backend/resources/scripts/hfd.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# Copyright © 2026 AtomGradient
-# 版权所有 © 2026 质子梯度（北京）科技有限公司
+# Copyright © https://hf-mirror.com/
 
 # Color definitions
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'; NC='\033[0m' # No Color
